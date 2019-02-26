55d5dd793ff1a1ed87d1b51fa7eb22ca41bbdb86
centerpush <dylancross100@gmail.com>
2019-02-26 15:57:15 -0600

    Set up the JWT stuff. Make sure you bundle after pulling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/temptations" => "temptations#index"
-    post "/temptations" => "temptations#create"
-    get "/temptations/:id" => "temptations#show"
-    patch "/temptations/:id" => "temptations#update"
-    delete "/temptations/:id" => "temptations#destroy"
+    get '/temptations' => 'temptations#index'
+    post '/temptations' => 'temptations#create'
+    get '/temptations/:id' => 'temptations#show'
+    patch '/temptations/:id' => 'temptations#update'
+    delete '/temptations/:id' => 'temptations#destroy'
 
-    post "/users" => "users#create"
+    post '/users' => 'users#create'
+
+    post '/sessions' => 'sessions#create'
   end
 end
