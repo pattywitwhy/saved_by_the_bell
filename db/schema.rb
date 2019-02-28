50672b6bba4abf0f5a80ef66103c3f1175dbd861
centerpush <dylancross100@gmail.com>
2019-02-28 15:56:11 -0600

    Made associations between user and temptations/goals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_201744) do
+ActiveRecord::Schema.define(version: 2019_02_28_214238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_201744) do
     t.integer "dollar_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "temptations", force: :cascade do |t|
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_201744) do
     t.boolean "Thursday"
     t.boolean "Friday"
     t.boolean "Saturday"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,48 +8,75 @@ User.create!(
 Temptation.create!(
                     name: "coffee",
                     cost: 5,
-                    # frequency: "1",
-                    time: "7:00"
+                    Sunday: true,
+                    Monday: true,
+                    Tuesday: true,
+                    Wednesday: true,
+                    Thursday: true,
+                    Friday: true,
+                    Saturday: true,
+                    user_id: 1
                   )
 
 Temptation.create!(
                     name: "lunch",
                     cost: 15,
-                    # frequency: "5",
-                    time: "12:00"
+                    Sunday: true,
+                    Monday: true,
+                    Tuesday: true,
+                    Wednesday: true,
+                    Thursday: true,
+                    Friday: true,
+                    Saturday: true,
+                    user_id: 1
                     )
 
 Temptation.create!(
                     name: "uber_morning",
                     cost: 25,
-                    # frequency: "5",
-                    time: "8:00"
+                    Sunday: true,
+                    Monday: true,
+                    Tuesday: true,
+                    Wednesday: true,
+                    Thursday: true,
+                    Friday: true,
+                    Saturday: true,
+                    user_id: 1
                     )
 
 Temptation.create!(
                     name: "uber_night",
                     cost: 30,
-                    # frequency: "5",
-                    time: "17:00"
+                    Sunday: true,
+                    Monday: true,
+                    Tuesday: true,
+                    Wednesday: true,
+                    Thursday: true,
+                    Friday: true,
+                    Saturday: true,
+                    user_id: 1
                     )
 
 Goal.create!(
              name: "wedding",
              start: "2019-2-26",
              end: "2020-2-26",
-             dollar_amount: 20000
+             dollar_amount: 20000,
+             user_id: 1
              )
 
 Goal.create!(
              name: "New_Zealand",
              start: "2019-2-26",
              end: "2020-3-1",
-             dollar_amount: 3000
+             dollar_amount: 3000,
+             user_id: 1
             )
 
 Goal.create!(
              name: "car",
              start: "2019-2-26",
              end: "2024-5-15",
-             dollar_amount: 4000
+             dollar_amount: 4000,
+             user_id: 1
              )
